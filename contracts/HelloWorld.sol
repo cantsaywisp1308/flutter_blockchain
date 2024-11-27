11185a9d4aca6141e71a32184fa95d4ae4e850e5
cantsaywisp1308 <duykhang.trinh1308@gmail.com>
2024-11-26 19:16:56 -0800

    init blockchain

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.21;
+
+contract HelloWorld {
+
+    string public yourName;
+
+    constructor() public {
+        yourName = "Unknown";
+    }
+
+    function setName(string memory nm) public {
+        yourName = nm;
+    }
+}
